75ec6a733e5fe3e10ba45064c1a48fc3d25ee27d
Ihor Stavratii <igor.stavratiy@gmail.com>
2019-12-19 13:56:18 +0200

    Updates

---
+++ precache-manifest.af37efad317df80e5f8c5e76c3b45c9c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "969995cd2d90b41cb8646c79074d5729",
+    "url": "/react_dynamic-list-of-todos/index.html"
+  },
+  {
+    "revision": "81f175ca2465cc0fe47f",
+    "url": "/react_dynamic-list-of-todos/static/css/main.41a3de45.chunk.css"
+  },
+  {
+    "revision": "22695e05fe258c625d75",
+    "url": "/react_dynamic-list-of-todos/static/js/2.94caacf4.chunk.js"
+  },
+  {
+    "revision": "81f175ca2465cc0fe47f",
+    "url": "/react_dynamic-list-of-todos/static/js/main.7f8158b1.chunk.js"
+  },
+  {
+    "revision": "04977e40ef1cd22bcb98",
+    "url": "/react_dynamic-list-of-todos/static/js/runtime~main.b6f1ef06.js"
+  }
+]);
